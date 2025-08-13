4c86fc4371e14e78f92846254969eacc1eeedb1a
haomiao <zhanghm1994@gmail.com>
2025-08-13 17:02:47 +0800

    add the correct path

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,7 @@ async function enableMocking() {
   // 启动 worker
   return worker.start({
     serviceWorker: {
-      url: '${import.meta.env.BASE_URL}mockServiceWorker.js'
+      url: `${import.meta.env.BASE_URL}mockServiceWorker.js`
     },
     onUnhandledRequest: 'bypass',
   })
