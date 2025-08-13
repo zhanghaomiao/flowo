632c9f5677ebebb1531d7ded83976fb4c8a1ea1e
haomiao <zhanghm1994@gmail.com>
2025-08-13 16:30:14 +0800

    add base path for mock service

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -2,3 +2,9 @@ import { setupWorker } from 'msw/browser'
 import { handlers } from './handlers'
 
 export const worker = setupWorker(...handlers)
+
+worker.start({
+  serviceWorker: {
+    url: `${import.meta.env.BASE_URL}mockServiceWorker.js`,
+  },
+})
