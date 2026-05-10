df7bf93ffc2c63bf27741da06f1b6e9910661b0d
jmzhang1911 <jmzhang1911@gmail.com>
2026-05-10 21:49:06 +0800

    add

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.routing import APIRoute
+from fastapi_mcp import FastApiMCP
 
 from .api import api_router
 from .core.config import settings
@@ -13,18 +14,6 @@ from .core.pg_listener import pg_listener
 # 配置日志 - 放在所有导入之前
 # 在所有导入之后，配置日志
 logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(name)s:%(message)s")
-
-
-def use_route_names_as_operation_ids(app: FastAPI) -> None:
-    """
-    Simplify operation IDs so that generated API clients have simpler function
-    names.
-
-    Should be called only after all routes have been added.
-    """
-    for route in app.routes:
-        if isinstance(route, APIRoute):
-            route.operation_id = route.name  # in this case, 'read_items'
 
 
 @asynccontextmanager
@@ -40,6 +29,11 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+mcp = FastApiMCP(
+    app, include_operations=["running_workflows_count", "workflow_progress"]
+)
+
+mcp.mount_http()
 
 app.add_middleware(
     CORSMiddleware,
@@ -51,7 +45,9 @@ app.add_middleware(
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
-use_route_names_as_operation_ids(app)
+
+mcp.setup_server()
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
